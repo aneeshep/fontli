0852155dc38fe5e215201519e2195e1731a1c0a2
Sathishkumar Natesan <sathish.n@imaginea.com>
2012-01-17 12:04:07 +0530

    Almost Forgot about seeds.

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,6 +1,6 @@
 # Be sure to restart your server when you modify this file.
 
-Fontli::Application.config.session_store :cookie_store, key: '_typestry_session'
+Fontli::Application.config.session_store :cookie_store, key: '_fontli_session'
 
 # Use the database for sessions instead of the cookie-based default,
 # which shouldn't be used to store highly confidential information
