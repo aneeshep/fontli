bf6e5043eddfadeb8eb295077619e82d148c1ad9
Sathishkumar Natesan <sathish.n@imaginea.com>
2015-06-01 19:28:29 +0530

    Make feature branch deployable

--- config/deploy.rb
+++ config/deploy.rb
@@ -6,6 +6,7 @@ set :repo_url,  "git@github.com:Imaginea/fontli.git"
 
 # Default branch is :master
 # ask :branch, proc { `git rev-parse --abbrev-ref HEAD`.chomp }.call
+set :branch, ENV['BRANCH'] || 'master'
 
 # Default deploy_to directory is /var/www/my_app
 set :deploy_to, '/data/www/typestry'
