e6db61aabf9185114b8ab000e24e0d2e28e6d3d1
Priya Rani <priya.rani@imaginea.com>
2016-03-18 16:38:07 +0530

    Fixed Apple push notification and updated expired certs

--- app/models/api_session.rb
+++ app/models/api_session.rb
@@ -10,34 +10,39 @@ class ApiSession
   index :auth_token => 1, :device_id => 1
 
   SESSION_EXPIRY_TIME = 40.weeks
-
+  
   belongs_to :user, :index => true
-
+  
   validates :user_id, :device_id, :presence => true
-
+  
   def self.[](token, devic_id)
     where(:auth_token => token, :device_id => devic_id).first
   end
-
+  
   def activate
     self.auth_token = generate_rand(16)
     self.expires_at = current_time + SESSION_EXPIRY_TIME
     self.save ? CGI.escape(token_str) : [nil, :unable_to_save]
   end
-
+  
   def deactivate
     self.auth_token = nil
     self.expires_at = current_time
-    self.save || [nil, :unable_to_save]
+    
+    if self.save
+      user.update_attribute(:iphone_token, nil)
+    else
+      [nil, :unable_to_save]
+    end
   end
-
+  
   def active?
     self.expires_at > current_time
   end
-
-private
+  
+  private
+  
   def token_str
     self.auth_token + '||' + self.device_id
   end
-
 end
